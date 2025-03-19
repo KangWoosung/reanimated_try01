a8a70d15fd1409ec411b893541f151704832d4f3
KangWoosung <wkang386@gmail.com>
2025-03-19 09:35:09 +0900

    Init

---
+++ commands.sh
@@ -0,0 +1,64 @@
+## 2025-03-17 06:14:12
+## Init Project
+npx create-expo-app@latest . --template default
+
+## Tailwind CSS & Reanimated
+npx expo install nativewind tailwindcss@^3.4.17 react-native-reanimated@3.16.2 react-native-safe-area-context
+
+## config Tailwind
+npx tailwindcss init
+
+## Edit config files as below
+## https://until.blog/@ganymedian/-1--react-native-%EC%A4%80%EB%B9%84%EC%9E%91%EC%97%85#79caf909-a753-432a-9116-26a716041601
+
+## Tailwind-Merge & clsx & Class Varience Authority 
+npx expo install tailwindcss clsx 
+npx expo install tailwind-merge
+## 2025-03-17 07:49:57
+## cva install occurs an error
+## npx expo install class-varience-authority 
+
+## Reanimated
+## npx expo install react-native-reanimated react-native-safe-area-context
+
+## Edit config files as below
+## https://www.notion.so/gunymedian/5-Reanimated-Native-APIs-140fc37aff268019960ff5c4b5b3ca4d?pvs=97#1b3fc37aff2680e8b550dbcb3a6576e3
+
+## Gesture Handler
+npx expo install react-native-gesture-handler
+
+## Gorhom Bottom Sheet
+npx expo install @gorhom/bottom-sheet@^5
+
+## Zustand
+npx expo install zustand
+
+## Zod
+npx expo install zod
+
+## Tanstack Query
+npx expo install @tanstack/react-query
+
+## MMKV
+npx expo install react-native-mmkv
+
+## RNR Primitives
+npx expo install @rn-primitives/slot
+npx expo install @rn-primitives/types
+npx expo install @rn-primitives/utils
+npx expo install @rn-primitives/portal
+npx expo install @rn-primitives/hooks
+
+
+## clear the cache
+npx expo start -c
+
+## prebuild
+npx expo prebuild
+
+
+
+
+
+
+
